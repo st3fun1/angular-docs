55a49ff420066be6eb2d3417d8e22041d0c4fe2d
Stefan Timosenco <12414812+st3fun1@users.noreply.github.com>
2025-01-23 12:01:27 +0200

    updated angular to version 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LayoutModule } from './modules/layout/layout.module';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from './modules/shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {
   BrowserAnimationsModule,
   NoopAnimationsModule,
@@ -15,23 +15,16 @@ import {
 import { RxjsDocModule } from './modules/rxjs-docs/rxjs-docs.module';
 import { PerformanceModule } from './modules/performance/performance.module';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    CommonModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    NoopAnimationsModule,
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    LayoutModule,
-    RouterModule,
-    SharedModule,
-    RxjsDocModule,
-    PerformanceModule,
-  ],
-
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [CommonModule,
+        BrowserAnimationsModule,
+        NoopAnimationsModule,
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        LayoutModule,
+        RouterModule,
+        SharedModule,
+        RxjsDocModule,
+        PerformanceModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
